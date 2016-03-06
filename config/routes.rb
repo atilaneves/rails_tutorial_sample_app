349503fb1b4424177029173d0cd8fe718147bdd2
Atila Neves <atila.neves@gmail.com>
2016-03-06 16:24:46 +0100

    Integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
