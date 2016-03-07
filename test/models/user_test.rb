e2403779304fc8221e22e83d69130c099f4a6cae
Atila Neves <atila.neves@gmail.com>
2016-03-07 12:00:08 +0100

    Email regex validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,21 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+   valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+   valid_addresses.each do |addr|
+     @user.email = addr
+     assert @user.valid?, "#{addr.inspect} should be valid"
+   end
+  end
+
+   test "email validation should reject invalid addresses" do
+     addrs = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+     addrs.each do |addr|
+       @user.email = addr
+       assert_not @user.valid?, "#{addr.inspect} should be invalid"
+     end
+   end
 end
