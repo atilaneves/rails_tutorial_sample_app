2f69c0985dae8b0394c86daaf505e2d1f5cd5ee3
Atila Neves <atila.neves@gmail.com>
2016-03-09 15:34:00 +0100

    Testing stuff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,26 @@ Minitest::Reporters.use!
 # Base clas for all tests
 class ActiveSupport::TestCase
   fixtures :all
+
+  def is_logged_in?
+    defined?(session) && !session[:user_id]
+  end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email,
+                                  password: password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
