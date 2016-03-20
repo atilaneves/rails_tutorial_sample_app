67552c4a3886380ea0db7116d39189c13c4d4e33
Atila Neves <atila.neves@gmail.com>
2016-03-20 15:35:24 +0100

    User and Micropost models are associated

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
 
+  has_many :microposts
+
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
